b8f01a852b2c2a6ea21bcce3de8bd0c61aa60ec7
Yaseen fehad <yaseenfehad@gmail.com>
2024-01-22 14:59:29 +0000

    tweeked the code after troubleshooting and added and on click function to ensure the data etered in the text area is being stored and pulled from the local storage

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -1,30 +1,26 @@
 var planWorkday = [
-    { time: "9 AM", 
-        event: "" },
-    { time: "10 AM", 
-        event: "" },
-    { time: "11 AM", 
-        event: "" },
-    { time: "12 PM", 
-        event: "" },
-    { time: "1 PM", 
-        event: "" },
-    { time: "2 PM", 
-        event: "" },
-    { time: "3 PM", 
-        event: "" },
-    { time: "4 PM", 
-        event: "" },
-    { time: "5 PM", 
-        event: "" },
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
 ];
 
 var today = dayjs().format("dddd, MMMM, D, YYYY ");
 $("#currentDay").text(today);
 
-planWorkday.forEach(function(timeBlock, index) {
+// Retrieve data from local storage
+var storedData = JSON.parse(localStorage.getItem("workDay")) || [];
+
+planWorkday.forEach(function (timeBlock, index) {
     var timeLabel = timeBlock.time;
     var blockColor = colorRow(timeLabel);
+    var storedEvent = storedData[index] ? storedData[index].event : ""; // Get stored event
+
     var row =
         '<div class="time-block" id="' +
         index +
@@ -33,21 +29,33 @@ planWorkday.forEach(function(timeBlock, index) {
         '</div><textarea class="form-control ' +
         blockColor +
         '">' +
-        timeBlock.event +
+        storedEvent +
         '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
-    
     $(".container").append(row);
 });
 
 function colorRow(time) {
     var planNow = dayjs();
-    var planEntry = dayjs(time, "h A"); 
+    var planEntry = dayjs(time, "h A");
     if (planNow.isBefore(planEntry)) {
-      return "future";
+        return "future";
     } else if (planNow.isAfter(planEntry)) {
-      return "past";
+        return "past";
     } else {
-      return "present";
+        return "present";
     }
 }
+
+$(".container").on("click", ".saveBtn", function () {
+    var blockID = parseInt(
+        $(this).closest(".time-block").attr("id")
+    );
+    var userEntry = $.trim(
+        $(this).parent().siblings("textarea").val()
+    );
+    planWorkday[blockID].event = userEntry;
+
+    // Update local storage
+    localStorage.setItem("workDay", JSON.stringify(planWorkday));
+});
